dc6c6a67da5acd0017ceed727d0dde3f3f01bd31
aizenstack <aizendevelopment21@gmail.com>
2025-10-25 15:08:16 +0700

    fix index

--- src/index.js
+++ src/index.js
@@ -1,16 +1,15 @@
 const express = require('express');
-const cors = require('cors')
+const cors = require('cors');
 const path = require("path");
-const dotenv = require('dotenv')
-const authRoutes = require('../router/authRoutes')
-const authMiddleware = require('../router/utils/authMiddleware')
+const dotenv = require('dotenv');
+const authRoutes = require('../router/authRoutes');
+const authMiddleware = require('../router/utils/authMiddleware');
 const newsRoutes = require('../router/newsRoutes');
 const uploadsRoutes = require('../router/uploadRoutes');
 const categoriesRoutes = require('../router/categoriesRoutes');
 const daysRoutes = require('../router/daysRoutes');
-const jimpitRoutes = require('../router/jimpitRoutes')
-const ressonRoutes = require('../router/ressonRoutes')
-
+const jimpitRoutes = require('../router/jimpitRoutes');
+const ressonRoutes = require('../router/ressonRoutes');
 
 dotenv.config();
 
@@ -22,29 +21,26 @@ app.use(express.urlencoded({ extended: true }));
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 app.use(express.json());
 
+app.use('/nomfour', authRoutes);
+app.use('/nomfour', newsRoutes);
+app.use('/nomfour', uploadsRoutes);
+app.use('/nomfour', categoriesRoutes);
+app.use('/nomfour', daysRoutes);
+app.use('/nomfour', jimpitRoutes);
+app.use('/nomfour', ressonRoutes);
+
 app.get('/', (req, res) => {
-    res.send('')
-    console.log('server success running')
-    res.status(200).end();
-})
+  console.log('server success running');
+  res.status(200).send('Server is running');
+});
+
 app.use((req, res) => {
   res.status(404).json({ message: "Endpoint not found" });
 });
+
 app.use((err, req, res, next) => {
-//   console.error(err.stack);
+  console.error(err);
   res.status(500).json({ message: "Internal server error" });
 });
 
-app.use('/nomfour',authRoutes);
-app.use('/nomfour',newsRoutes);
-app.use('/nomfour',uploadsRoutes);
-app.use('/nomfour',categoriesRoutes);
-app.use('/nomfour',daysRoutes);
-app.use('/nomfour',jimpitRoutes);
-app.use('/nomfour',ressonRoutes);
-
-
-// app.listen(PORT, () => {
-//     console.log('server is running');
-// })
 module.exports = app;
