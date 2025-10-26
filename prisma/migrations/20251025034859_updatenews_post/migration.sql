618dab4084910051eb74b272dc56fe72a51e1490
aizenstack <aizendevelopment21@gmail.com>
2025-10-26 18:10:03 +0700

    Memperbarui .gitignore dan membersihkan file yang tidak perlu di-track

--- prisma/migrations/20251025034859_updatenews_post/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[slug]` on the table `news_post` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX "news_post_slug_key" ON "news_post"("slug");
