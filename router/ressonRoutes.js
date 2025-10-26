f7182c1d85c7a3d80f2fe1a35bf7f31756814c34
aizenstack <aizendevelopment21@gmail.com>
2025-10-26 15:13:06 +0700

    update-resson-endpoint

--- router/ressonRoutes.js
+++ router/ressonRoutes.js
@@ -1,8 +1,8 @@
-const express = require('express');
-const {createResson} = require('../controller/ressonController');
-
+const express = require("express");
 const router = express.Router();
+const { createResson, getAllRessons } = require("../controller/ressonController");
 
-router.post('/resson', createResson);
+router.post("/resson", createResson);
+router.get("/resson", getAllRessons);
 
 module.exports = router;
