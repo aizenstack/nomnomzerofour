89c813c569a85010d54087f845adc2f547ff0f83
aizenstack <aizendevelopment21@gmail.com>
2025-10-28 19:54:01 +0700

    back

--- router/jimpitRoutes.js
+++ router/jimpitRoutes.js
@@ -6,8 +6,11 @@ const {
   getAllJimpitTeams,
   getJimpitTeamById,
 } = require("../controller/jimpitControlller");
+const { authMiddleware } = require('./utils/authMiddleware');
 
 const router = express.Router();
+
+router.use(authMiddleware);
 
 router.get("/jimpit", getAllJimpitTeams);
 router.get("/jimpit/:id", getJimpitTeamById);
