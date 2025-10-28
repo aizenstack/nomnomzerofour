84efc2934f7dbf60a82e112de9a3f069cc0fde6b
aizenstack <aizendevelopment21@gmail.com>
2025-10-28 16:19:16 +0700

    add middleware

--- router/notedRoutes.js
+++ router/notedRoutes.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const {authMiddleware} = require('../router/utils/authMiddleware')
 const {getAllNoted, createNoted, updateNoted, deleteNoted} = require('../controller/notedController');
 
 const router = express.Router();
 
+router.use(authMiddleware)
 router.get('/noteds', getAllNoted);
 router.post('/noteds', createNoted);
 router.patch('/noteds/:id', updateNoted);
