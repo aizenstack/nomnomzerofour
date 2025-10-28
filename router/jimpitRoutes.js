63353936c971dc72dc1642c2d51e881ee21c37b4
aizenstack <aizendevelopment21@gmail.com>
2025-10-28 21:14:55 +0700

    fix it

--- router/jimpitRoutes.js
+++ router/jimpitRoutes.js
@@ -6,7 +6,6 @@ const {
   getAllJimpitTeams,
   getJimpitTeamById,
 } = require("../controller/jimpitControlller");
-const { authMiddleware } = require('./utils/authMiddleware');
 
 const router = express.Router();
 
@@ -14,8 +13,8 @@ const router = express.Router();
 
 router.get("/jimpit", getAllJimpitTeams);
 router.get("/jimpit/:id", getJimpitTeamById);
-router.post("/jimpit", authMiddleware, createdJimpitTeams);
-router.patch("/jimpit/:id", authMiddleware, updateJimpitTeams);
-router.delete("/jimpit/:id", authMiddleware, deleteJimpitTeams);
+router.post("/jimpit", createdJimpitTeams);
+router.patch("/jimpit/:id", updateJimpitTeams);
+router.delete("/jimpit/:id", deleteJimpitTeams);
 
 module.exports = router;
